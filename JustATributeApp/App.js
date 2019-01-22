08ce37e0cb3ffcfedc92ad3f76e31278997a38b0
syed <syed@digitribe.in>
2019-01-22 19:51:01 +0530

    Initial Commit

---
+++ JustATributeApp/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, TouchableHighlight} from 'react-native';
+import PresentationalComponent from './PresentationalComponent';
+import List from './List';
+
+const App = () => {
+  return (
+     <List />
+  )
+}
+export default App
